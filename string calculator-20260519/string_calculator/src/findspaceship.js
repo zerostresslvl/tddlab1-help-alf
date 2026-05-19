3b6ecacd38b15891ce48ce96e11c24897b2f58fa
zerostresslvl <zerostresslvl@gmail.com>
2026-05-19 13:17:49 +0200

    instead of hardcoding the indexes of the coordinates inside -> we go through each new line till we find the row that has X in it -> after that we count the count it took us to get to the line with X inside it -> loop checks the exact position its in -> so in case of 7 goes till 7 -> and for the Y we calculate the total amount of rows.length - 1 because its an array and index 0 exists - the actual row where we found it -> we flipped the bottom row to 0 instead of top  and then the result of that gives us the y coordinates that we were looking for accordingly

--- string calculator-20260519/string_calculator/src/findspaceship.js
+++ string calculator-20260519/string_calculator/src/findspaceship.js
@@ -5,12 +5,16 @@ if(!map || map.length === 0) {
     return "Spaceship lost forever.";
 }
 
-  if (map.indexOf('X') !== -1) {
-    return [0, 0];
+  var rows = map.split('\n');
+
+  for (var row = 0; row < rows.length; row++) {
+    var col = rows[row].indexOf('X');
+    if (col !== -1) {
+      var y = rows.length - 1 - row;
+      return [col, y];
+    }
   }
 
   return "Spaceship lost forever.";
-
-
 };
 
