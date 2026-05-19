cf99c4b62b6ff51ae3fadbf6eda1f1f620980bc4
zerostresslvl <zerostresslvl@gmail.com>
2026-05-19 12:45:50 +0200

    passing green test case for 0, 0 coordinates now returns the coordinates if we have 0,0 as coordinates

--- string calculator-20260519/string_calculator/src/findspaceship.js
+++ string calculator-20260519/string_calculator/src/findspaceship.js
@@ -5,5 +5,12 @@ if(!map || map.length === 0) {
     return "Spaceship lost forever.";
 }
 
+  if (map.indexOf('X') !== -1) {
+    return [0, 0];
+  }
+
+  return "Spaceship lost forever.";
+
+
 };
 
