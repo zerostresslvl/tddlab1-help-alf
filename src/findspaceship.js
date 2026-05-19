aca4100e4b78924fc78e5ff8bebeffab1c695bd6
zerostresslvl <zerostresslvl@gmail.com>
2026-05-19 12:16:52 +0200

    now we added an additional string on the return type on empty maps so it knows to return spaceship lost forever and accordingly on the empty return type it returns what we wanted it to and the test is now green and passes

--- src/findspaceship.js
+++ src/findspaceship.js
@@ -1,7 +1,9 @@
 function FindSpaceship() {}
 
 FindSpaceship.prototype.find = function(map) {
-  //
+if(!map || map.length === 0) {
+    return "Spaceship lost forever.";
+}
 };
 
 module.exports = FindSpaceship;
